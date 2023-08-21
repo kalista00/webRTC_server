a936401ce0feda4dbb8784a0e0f657675c75a164
김창희 <122184547+kalista00@users.noreply.github.com>
2023-08-22 03:18:43 +0900

    Add files via upload

---
+++ spring-boot-webrtc-peer2peer-master/src/main/java/com/gucardev/springbootwebrtcpeer2peer/MainController.java
@@ -0,0 +1,14 @@
+//package com.gucardev.springbootwebrtcpeer2peer;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//@Controller
+//@RequestMapping("/*")
+//public class MainController {
+//	
+//	@RequestMapping("conn")
+//	public String conn() {
+//		return "index.html";
+//	}
+//}
